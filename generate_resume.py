dd6cb12ebdb59cd44e05fd0b82bec365dd239ef1
mbahgatTech <mazenbahgat@outlook.com>
2024-11-04 11:08:49 -0500

    refactor params

--- generate_resume.py
+++ generate_resume.py
@@ -4,8 +4,8 @@ def load_data(filename, is_json=False):
     with open(filename, "r") as file:
         return json.load(file) if is_json else file.read()
 
-def define_variable(latex, name, value):
-    return latex + f"\\def\\{name}{{{value}}}\n"
+def define_variable(name, value):
+    return f"\\def\\{name}{{{value}}}\n"
 
 def retrieve_value(key, json_config):
     value = json_config[key]
@@ -15,43 +15,50 @@ def retrieve_value(key, json_config):
 
     return value
 
-def define_variables(latex, json_config, filter=lambda key, value: True):
+def define_variables(json_config, filter=lambda key, value: True):
     if json_config is None:
         return
     
-    if latex is None:
-        latex = ""
+    latex = ''
 
     for key in json_config:
         var_value = retrieve_value(key, json_config)
         if (not filter(key, var_value)): continue
 
-        latex = define_variable(latex, key, var_value)
+        latex += define_variable(key, var_value)
     
     return latex
 
-def load_subheading(latex, subheading_config):
-    latex = define_variables(latex, subheading_config)
+def load_heading(heading_config):
+    latex = define_variables(heading_config)
+    latex += load_data('./components/heading.tex')
+    return latex
+
+def load_subheading(subheading_config):
+    latex = define_variables(subheading_config)
     latex += load_data('./components/subheading.tex')
     return latex
 
-def load_subheadings(latex, section_config):
+def load_subheadings(section_config):
+    latex = ''
+
     for subheading in section_config['subheadings']:
-        latex = load_subheading(latex, subheading)
+        latex += load_subheading(subheading)
 
     return latex
 
-def load_section(latex, section_config):
-    latex = define_variables(latex, section_config, lambda key, value: key == "sectionName")
+def load_section(section_config):
+    latex = define_variables(section_config, lambda key, value: key == "sectionName")
     latex += load_data('./components/section.tex')
-    latex = load_subheadings(latex, section_config)
+    latex += load_subheadings(section_config)
     latex += load_data('./components/sectionEnd.tex')
-    latex += load_data('./components/end.tex')    
     return latex
 
-def load_sections(latex, body_config):
+def load_sections(body_config):
+    latex = ''
+
     for section in body_config:
-        latex = load_section(latex, section)
+        latex += load_section(section)
 
     return latex
 
@@ -60,11 +67,12 @@ def load_content():
     latex += load_data('./components/base.tex')
     
     heading_config = load_data('./config/heading.json', True)
-    latex = define_variables(latex, heading_config)
-    latex += load_data('./components/heading.tex')
+    latex += load_heading(heading_config)
 
     body_config = load_data('./config/sections.json', True)
-    latex = load_sections(latex, body_config)
+    latex += load_sections(body_config)
+    latex += load_data('./components/end.tex')
+
     return latex
 
 def write_latex_resume(latex):
