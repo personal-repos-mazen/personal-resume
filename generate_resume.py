3fe9a4f71e47539c62a68d8afae6363206a902e2
mbahgatTech <mazenbahgat@outlook.com>
2024-11-03 21:25:51 -0500

    base template components

---
+++ generate_resume.py
@@ -0,0 +1,11 @@
+import json
+
+def load_data(filename="resume.json"):
+    with open(filename, "r") as file:
+        return json.load(file)
+
+def generate_section(data):
+    return
+
+def save_section(content, filename):
+    return
